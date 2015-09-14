6ea12961e3c9211f1b45acd74bc3eba8e3976545
Halana Thais Silva Bezerra de Melo <halanatmelo@gmail.com>
2015-09-14 20:35:54 -0300

    Create infogas.c

---
+++ infogas.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*Informar qual a hora de saida, capacidade do tanque de combustível, kilometragem marcada no odômetro. 
+  A cada parada informar a kilometragem percorrica, o quanto abasteceu o tanque e a hora da parada. 
+  Perguntar após cada abastecimento se é a última parada (y/n), caso sim mostrar na tela os dados finais 
+  como velocidade média otal e consumo total, e os relatórios parciais anteriores.*/
+  
+  int main(void)
+{
+
+     /* Variaveis. */
+
+
+
+
+  return EXIT_SUCESS;
+}
