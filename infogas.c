9637c4a81211d80b262abc8e10efb2ec4622c85e
Halana Thais Silva Bezerra De Melo <halanatmelo@gmail.com>
2015-09-15 15:53:39 -0300

    alteração inicial do programa

--- infogas.c
+++ infogas.c
@@ -1,18 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/*Informar qual a hora de saida, capacidade do tanque de combustível, kilometragem marcada no odômetro. 
-  A cada parada informar a kilometragem percorrica, o quanto abasteceu o tanque e a hora da parada. 
-  Perguntar após cada abastecimento se é a última parada (y/n), caso sim mostrar na tela os dados finais 
-  como velocidade média otal e consumo total, e os relatórios parciais anteriores.*/
-  
-  int main(void)
+int main(void)
 {
-
-     /* Variaveis. */
-
-
-
-
-  return EXIT_SUCESS;
+        // Variáveis //
+    float l, h, k, p,km_atual, f, g, t, a;
+    int y, m;
+        // Entrada de dados //                    
+    printf ("Saida as hh:mm : \n ");
+    scanf ("%f", &h);         
+    printf ("Informe a kilometragem atual: \n ");
+    scanf ("%f", &k);                          
+    printf ("Informe a capacidade do tanque: \n ");
+    scanf ("%f", &l);
+    printf ("Previsao de hora de chegada ao destino hh:mm: \n ");
+    scanf ("%f", &g);
+    printf ("Inserir dados da parada de abastecimento! \n");
+    
+   while (m<n)
+   {
+       printf ("Informe a hora da parada em hh:mm : \n ");
+       scanf ("%f", &p);        
+       printf ("Informe a kilometragem atual: \n ");
+       scanf ("%f", &km_atual);                                                      
+       printf ("Informe a quantidade de litros atual de combustivel: \n ");
+       scanf ("%f", &t);
+       f=(y-k)/(p-h);
+       printf ("Velocidade media entre paradas: \n ", "%f");
+       printf ("Consumo parcial: %f \n ", (l-t) );
+       printf("Previsão hora de chegada: %f \n ", a-(p-h) );
+       printf("Ultima parada? y (pressione  1) n (pressione 2);
+       scanf ("%d", &y);
+           if ( y==1)
+           { 
+               m=4;
+           }
+   } 
+         
+ return 0;
 }
