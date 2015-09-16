d453a7899ee99cde7702dc16c5e8c1b69f2558f6
Priscila De Sousa Silva <cila_007@hotmail.com>
2015-09-15 21:21:47 -0300

    alteração codigo

--- infogas.c
+++ infogas.c
@@ -4,7 +4,7 @@
 int main(void)
 {
         // Variáveis //
-    float litros, km_saida, hora_parada, km_atual, vm, hora2, f, a;
+    float litros, km_saida, hora_parada, km_atual, vm, hora2, litros_abastecidos, a;
     int y, minutos, hora;
         // Entrada de dados //                    
     printf ("Informe a hora de saida: \n ");
